4871dfb2cb525017fd8683b1f015c339aa345c30
tekilaz1 <tselama18@gmail.com>
2019-11-14 14:11:36 +0200

    change file name
    Signed-off-by: tekilaz1 <tselama18@gmail.com>

---
+++ main.js
@@ -0,0 +1,16 @@
+const greet = function(name){
+    console.log("Hello there, " + name)
+  }
+
+  function cal(a, b){
+    return a - b 
+    }
+
+
+
+    function calc(a, b){
+      sum = a + b 
+      console.log( " the sum is", + sum)
+      sum1 = a - b 
+      console.log( " the subtraction is", + sum1)
+    }
